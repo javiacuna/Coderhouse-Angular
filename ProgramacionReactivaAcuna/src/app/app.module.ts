0eff61e3272f75094ff2b6ddac7f192cf8f11e3e
javiacuna <javier.acuna@mercadolibre.com>
2022-08-21 17:38:23 -0300

    Desafio

--- ProgramacionReactivaAcuna/src/app/app.module.ts
+++ ProgramacionReactivaAcuna/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AsyncComponent } from './components/async/async.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsyncComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ProgramacionReactivaAcuna/src/app/services/service.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+export interface Datos {
+  nombre: string;
+  edad: number;
+  email: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+  numeros: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  alumnos: Datos[] = [
+    { nombre: 'Javier', edad: 30, email: 'javi@gmail.com' },
+    { nombre: 'Juan', edad: 34, email: 'juan@gmail.com' },
+    { nombre: 'Celeste', edad: 54, email: 'celeste@gmail.com' },
+    { nombre: 'Marcos', edad: 23, email: 'marcos3@gmail.com' }
+  ]
+
+  constructor() { }
+
+  obtenerDatosAlumnos(): Observable<Datos[]> {
+    return of(this.alumnos);
+  }
+
+  obtenerNumeros(): Observable<number[]> {
+    return of(this.numeros);
+  }
+
+  obtenerDatosPromesa(): Promise<Datos[]> {
+    return new Promise((resolve, reject) => {
+      resolve(this.alumnos);
+      reject('Error');
+    });
+  }
+}
